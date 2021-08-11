ee6b91416c3b6b30643c67d69fb4e120c10353fe
Макс Сухарев <mazy_max@mail.ru>
2021-08-11 21:24:05 +0800

    Перенес клас геометрии

--- AntaresUtilsNetFramework/AntaresUtils.cs
+++ AntaresUtilsNetFramework/AntaresUtils.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 
 namespace AntaresUtilsNetFramework
@@ -166,6 +167,27 @@ namespace AntaresUtilsNetFramework
                 cmd.ExecuteNonQuery();
                 cmd.Dispose();
             }
+        }
+    }
+
+    public class RecipeGeometry : IComparable<RecipeGeometry>
+    {
+        public string RecipeId;
+        public int LineId;
+        public int ItemType;
+        public int X;
+        public int Y;
+        public int Z;
+        public int Total;
+
+        public int CompareTo(RecipeGeometry other)
+        {
+            if (this.LineId > other.LineId) return 1;
+            else if (this.LineId > other.LineId) return -1;
+
+            if (this.ItemType > other.ItemType) return 1;
+            else if (this.ItemType > other.ItemType) return -1;
+            return 0;
         }
     }
 }
--- AntaresUtilsNetFramework/MainForm.cs
+++ AntaresUtilsNetFramework/MainForm.cs
@@ -313,25 +313,4 @@ namespace AntaresUtilsNetFramework
         public string GMID;
         public List<RecipeGeometry> ListOfrecipeGeometries;
     }
-
-    public class RecipeGeometry : IComparable<RecipeGeometry>
-    {
-        public string RecipeId;
-        public int LineId;
-        public int ItemType;
-        public int X;
-        public int Y;
-        public int Z;
-        public int Total;
-
-        public int CompareTo(RecipeGeometry other)
-        {
-            if (this.LineId > other.LineId) return 1;
-            else if (this.LineId > other.LineId) return -1;
-
-            if (this.ItemType > other.ItemType) return 1;
-            else if (this.ItemType > other.ItemType) return -1;
-            return 0;
-        }
-    }
 }
